1df3233dcb011ffec38defaebc94efe9915223ba
Xaxis <william.neeley@gmail.com>
2014-03-28 12:17:29 -0700

    Added immediate parallax initialization

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jQuery Scrollfire
 
-Version 1.1.0
+Version 1.2.0
 
 ## Summary
 
@@ -10,6 +10,16 @@ Allows useful callbacks to be fired upon elements scrolling into and out of view
 
 Wil Neeley ( [@wilneeley](http://twitter.com/wilneeley) / [trestlemedia.net](http://www.trestlemedia.net) / [github.com](https://github.com/Xaxis) )
 
+## Overview
+
+jQuery.scrollfire gives its users the ability to execute callbacks on a multitude of scroll based events.
+* ... easily animate elements with parallax effects.
+* ... add infinite scrolling via ajax to load content at set scroll positions.
+* ... call functions when an element(s) scrolls into and/or out of view (firing on the top or bottom of the element).
+* ... call functions based on scroll direction ONLY when an element is in view.
+* ... call functions based on scroll direction continuously.
+* ... fire code at any scroll position via the usage of element targeting in combination with specifying an offset.
+
 ## Usage
 
 Include `jquery.scrollfire.min.js` after jQuery.
@@ -18,51 +28,70 @@ Include `jquery.scrollfire.min.js` after jQuery.
 
 Often it is useful to have the ability to execute code upon the scrolling of an element(s) into and/or out of view. jQuery.scrollfire gives you this ability via four callback methods: `onTopIn`, `onTopOut`, `onBottomIn`, and `onBottomOut`.
 
-### Register Scrollfire on Elements
+### Initializing Scrollfire
 
 ```javascript
-// Example attached to DIVs with class .container
+// Example of using the once methods
 $('.container').scrollfire({
-    //offset: 200,              // Default offset setting
-    //topInOffset: 0,           // Further offset properties override the default offset setting
-    //topOutOffset: 0,          // ...
-    //bottomInOffset: 0,        // ...
-    //bottomOutOffset: 0,       // ...
-    onTopIn: function( elm ) {
-        $(elm).animate({width: "100%"}, 500, function() {
-            $(this).css('border', '2px dashed deeppink');
-        });
+    offset: 100,
+    //topInOffset: 0,
+    //topOutOffset: 0,
+    //bottomInOffset: 0,
+    //bottomOutOffset: 0,
+    onTopIn: function( elm, distance_scrolled ) {
+        $(elm).animate({opacity: 1}, 500);
     },
-    onTopOut: function( elm ) {
-        $(elm).animate({width: "100px"}, 500, function() {
-            $(this).css('border', '2px dashed deeppink');
-        });
+    onTopOut: function( elm, distance_scrolled ) {
+        $(elm).animate({opacity: 0.2}, 500);
     },
-    onBottomIn: function( elm ) {
-        $(elm).animate({width: "100%"}, 500, function() {
-            $(this).css('border', '2px solid black');
-        });
+    onBottomIn: function( elm, distance_scrolled ) {
+        $(elm).animate({opacity: 1}, 500);
     },
-    onBottomOut: function( elm ) {
-        $(elm).animate({width: "100px"}, 500, function() {
-            $(this).css('border', '2px solid black');
-        });
-    },
-    onScrollDown: function( elm, distance_scrolled, percentage_to_top ) {
-      var div = $(elm);
-      var div_height = div.height();
-      var parallax_pos = (div_height * from_top) - $('.parallax-cell').outerHeight();
-      $(elm).find('.parallax-cell').css('top', parallax_pos);
-    },
-    onScrollUp: function( elm, distance_scrolled, percentage_to_top ) {
-      var div = $(elm);
-      var div_height = div.height();
-      var parallax_pos = (div_height * from_top) - $('.parallax-cell').outerHeight();
-      $(elm).find('.parallax-cell').css('top', parallax_pos);
+    onBottomOut: function( elm, distance_scrolled ) {
+        $(elm).animate({opacity: 0.2}, 500);
     },
     onScroll: function( elm, distance_scrolled, scroll_direction ) {
-      //console.log('Happening continuously);
+        //console.log('Happening continuously while scrolling in either direction');
+    },
+    onScrollDown: function( elm, distance_scrolled, percentage_to_top ) {
+        //console.log('Continuously happening while scrolling down when element is in view!');
+    },
+    onScrollUp: function( elm, distance_scrolled, percentage_to_top ) {
+        //console.log('Continuously happening while scrolling up when element is in view!');
+    },
+    onScrollDownAlways: function( elm, distance_scrolled ) {
+        //console.log('Continuously happening while scrolling down!');
+    },
+    onScrollUpAlways: function( elm, distance_scrolled ) {
+        //console.log('Continuously happening while scrolling up');
     }
+});
+
+
+// Example of using parallax scrolling (Note: child must be positioned absolutely to relatively positioned parent)
+$('.parallax-cell').scrollfire({
+
+    // Parallax is disabled when element is 100 pixels away from scrolling out of view in either direction
+    offset: 100,
+    parallax: {
+
+        // The element the jQuery selected element is a direct child of
+        childOf: $('.container'),
+
+        // Parallax is 75% the window scrolling speed
+        speed: 0.75
+    },
+
+    // Additionally, you can still use any of the callbacks
+    onScroll: function( elm, distance_scrolled, direction ) {
+        //console.log('Happening continuously!');
+    }
+});
+
+
+// Button event handler - Remove registered scrollfire elements
+$('#1').click(function(e) {
+   $('.parallax-cell').scrollfire('remove');
 });
 ```
 
@@ -96,3 +125,7 @@ See `example.html` in examples folder.
 #### Version 1.1.0
 
 * added parallax scrolling methods: `onScrollDown`, `onScrollUp`, and `onScroll`
+
+### Version 1.2.0
+
+* added immediate parallax initialization
--- jquery.scrollfire.js
+++ jquery.scrollfire.js
@@ -17,14 +17,20 @@
       // The plugin's defaults
       defaults = {
 
-        // Executes CONTINUOUSLY when scrolling down AND while element is in view
-        onScrollDown: function() {},
-
-        // Executes CONTINUOUSLY when scrolling up AND while element is in view
-        onScrollUp: function() {},
-
         // Executes CONTINUOUSLY when scrolling in either direction
         onScroll: function() {},
+
+        // Executes CONTINUOUSLY when scrolling down WHILE element is in view
+        onScrollDown: function() {},
+
+        // Executes CONTINUOUSLY when scrolling up WHILE element is in view
+        onScrollUp: function() {},
+
+        // Executes CONTINUOUSLY when scrolling down
+        onScrollDownAlways: function() {},
+
+        // Executes CONTINUOUSLY when scrolling up
+        onScrollUpAlways: function() {},
 
         // Executes ONCE when element comes in from TOP (DOWN)
         onTopIn: function() {},
@@ -37,6 +43,16 @@
 
         // Executes ONCE when element goes out the BOTTOM (UP)
         onBottomOut: function() {},
+
+        // Parallax object definition
+        parallax: {
+
+          // The element the parallax scroll is to be relative to
+          childOf: null,
+
+          // The speed differential relative to the rate of scroll
+          speed: 1
+        },
 
         // Offsets
         offset: 0,
@@ -80,9 +96,11 @@
     var scroll_obj = {
       _guid: globals.guid,
       _element: plugin.element,
+      _on_scroll: plugin.options.onScroll,
       _on_scroll_down: plugin.options.onScrollDown,
       _on_scroll_up: plugin.options.onScrollUp,
-      _on_scroll: plugin.options.onScroll,
+      _on_scroll_down_always: plugin.options.onScrollDownAlways,
+      _on_scroll_up_always: plugin.options.onScrollUpAlways,
       _on_top_in: plugin.options.onTopIn,
       _on_top_in_once: true,
       _on_top_out: plugin.options.onTopOut,
@@ -95,7 +113,8 @@
       _top_in_offset: plugin.options.topInOffset,
       _top_out_offset: plugin.options.topOutOffset,
       _bottom_in_offset: plugin.options.bottomInOffset,
-      _bottom_out_offset: plugin.options.bottomOutOffset
+      _bottom_out_offset: plugin.options.bottomOutOffset,
+      _parallax: plugin.options.parallax
     };
 
     // Define the scroll handler
@@ -105,23 +124,20 @@
       var scroll_pos = $(window).scrollTop();
 
       // Reference the scroll direction
-      var scroll_dir = '';
+      var scroll_dir = (scroll_pos > globals.last_scroll_pos) ? 'down' : 'up';
 
       // Calculate the scroll difference (distance scrolled since last scroll event)
       var scroll_diff = Math.abs(globals.last_scroll_pos - scroll_pos);
 
-      // Scrolling Down
-      if (scroll_pos > globals.last_scroll_pos) {
-        scroll_dir = 'down';
-
-        // Iterate over our registration objects
-        $.each(globals.registry, function(index, value) {
+      // Fire callbacks on the registration objects
+      $.each(globals.registry, function(index, value) {
+        if (scroll_dir == 'down') {
 
           // Elements position from the top
           var elm_pos_top = $(value._element).offset().top;
 
           // Reference element position conditions
-          var onTopOut_true = scroll_pos >= (elm_pos_top + (value._top_out_offset ? value._top_out_offset : value._offset));
+          var onTopOut_true = scroll_pos >= (elm_pos_top + (value._top_out_offset ? -value._top_out_offset : -value._offset));
           var onBottomIn_true = (scroll_pos + $(window).outerHeight()) >= (elm_pos_top + (value._bottom_in_offset ? value._bottom_in_offset : value._offset));
 
           // Reference conditions that tell us if an element is visible in the screen
@@ -131,10 +147,13 @@
           // Fire the registered onScroll callback
           value._on_scroll( value._element, scroll_diff, scroll_dir );
 
-          // Fire the registered onScrollDown callback
+          // Fire the registered onScrollDownAlways callback
+          value._on_scroll_down_always( value._element, scroll_diff );
+
+          // Fire the registered onScrollDown callback and parallax functionality
           if ( visibleTop && visibleBottom ) {
 
-            // For user convenience, it is useful to pass a value to the callbacks that is the percentage distance an
+            // For user convenience, it is useful to pass a value to the callback that is the percentage distance an
             // element is from going out of view at the top of the window.
             var window_height = $(window).outerHeight();
             var to_top_distance = (elm_pos_top + $(value._element).outerHeight()) - scroll_pos;
@@ -143,6 +162,21 @@
 
             // Fire callback
             value._on_scroll_down( value._element, scroll_diff, perc_from_top );
+
+            // Execute parallax scroll relative to element
+            if (value._parallax.childOf && (to_top_distance >= value._offset)) {
+
+              // Parallax element's current position from top of container element
+              var parallax_elm_from_top = $(value._element).position().top;
+
+              // Position to move parallaxed element to
+              var parallax_pos = (parallax_elm_from_top - (scroll_diff * value._parallax.speed));
+
+              // Parallax scroll only while still within container element
+              if (parallax_pos >= 0) {
+                $(value._element).css('top', parallax_pos);
+              }
+            }
           }
 
           // Fire the registered onTopOut callback
@@ -158,21 +192,14 @@
             value._on_bottom_in_once = false;
             value._on_bottom_in( value._element, scroll_diff );
           }
-        });
-
-      // Scrolling Up
-      } else {
-        scroll_dir = 'up';
-
-        // Iterate over our registration objects
-        $.each(globals.registry, function(index, value) {
+        } else {
 
           // Bottom of element's position from the top
           var elm_pos_top = $(value._element).offset().top + $(value._element).outerHeight();
 
           // Reference element position conditions
-          var onTopIn_true = scroll_pos <= (elm_pos_top + (value._top_in_offset ? value._top_in_offset : value._offset));
-          var onBottomOut_true = (scroll_pos + $(window).outerHeight()) <= (elm_pos_top + (value._bottom_out_offset ? value._bottom_out_offset : value._offset));
+          var onTopIn_true = scroll_pos <= (elm_pos_top + (value._top_in_offset ? -value._top_in_offset : -value._offset));
+          var onBottomOut_true = (scroll_pos + $(window).outerHeight()) <= (elm_pos_top + (-value._bottom_out_offset ? -value._bottom_out_offset : value._offset));
 
           // Reference conditions that tell us if an element is visible in the screen
           var visibleTop = scroll_pos < elm_pos_top;
@@ -181,10 +208,13 @@
           // Fire the registered onScroll callback
           value._on_scroll( value._element, scroll_diff, scroll_dir );
 
+          // Fire the registered onScrollUpAlways callback
+          value._on_scroll_up_always( value._element, scroll_diff );
+
           // Fire the registered onScrollUp callback
           if ( visibleTop && visibleBottom ) {
 
-            // For user convenience, it is useful to pass a value to the callbacks that is the percentage distance an
+            // For user convenience, it is useful to pass a value to the callback that is the percentage distance an
             // element is from going out of view at the top of the window.
             var window_height = $(window).outerHeight();
             var to_top_distance = (elm_pos_top) - scroll_pos;
@@ -193,6 +223,25 @@
 
             // Fire callback
             value._on_scroll_up( value._element, scroll_diff, perc_from_top );
+
+            // Execute parallax scroll relative to element
+            if (value._parallax.childOf) {
+
+              // Parallax element's current position from top of container element
+              var parallax_elm_from_top = $(value._element).position().top;
+
+              // Position to move parallaxed element to
+              var parallax_pos = (parallax_elm_from_top + (scroll_diff * value._parallax.speed));
+
+              // Calculate the position offset induced by element padding
+              var container_padding = value._parallax.childOf.css('padding-bottom').replace(/px/g, "");
+              var parallax_offset = (container_padding == '0') ? $(value._element).outerHeight() : $(value._element).height();
+
+              // Parallax scroll only while still within container element
+              if ((parallax_pos + parallax_offset - container_padding) <= value._parallax.childOf.height()) {
+                $(value._element).css('top', parallax_pos);
+              }
+            }
           }
 
           // Fire the registered onTopIn callback
@@ -208,8 +257,8 @@
             value._on_bottom_out_once = false;
             value._on_bottom_out( value._element, scroll_diff );
           }
-        });
-      }
+        }
+      });
 
       // Update the global scroll position
       globals.last_scroll_pos = scroll_pos;
